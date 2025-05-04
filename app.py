701a62113a2f79fe97bd7a2dc76f97635708a1b7
Shuba S <shuba9902@gmail.com>
2025-05-04 18:18:31 +0530

    no changes yet

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template
+import mysql.connector
+from config import Config
 
 # Create a Flask application instance
 app = Flask(__name__)
 
+app.config.from_object(Config)
+
+#connect database cred using config file
+def get_db_connector():
+    return mysql.connector.connect(
+        host=Config.DB_HOST,
+        user=Config.DB_USER,
+        password=Config.DB_PASSWORD,
+        database=Config.DB_NAME
+        )
 # Define a route for the root URL
 @app.route('/')
 def login():
